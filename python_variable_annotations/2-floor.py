567c9b63cbf8e869bb49bf2a85b6d5e999162106
ikerikks <ikerikks@gmail.com>
2023-09-22 10:38:06 +0200

    documentation

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,6 +2,14 @@
 
 import math
 
+"""
+    floor function
+    Args:
+        n: float number
+    Returns:
+        floor of float number
+"""
+
 
 def floor(n: float) -> int:
     return math.floor(n)
