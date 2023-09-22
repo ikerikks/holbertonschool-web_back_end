6142dcd140a4ed2c59921c7d12d5d2f4f3366be2
ikerikks <ikerikks@gmail.com>
2023-09-22 10:31:05 +0200

    documentation update

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
-""" add two numbers """
+"""
+    add two numbers
+    Arguments:
+        a: number
+        b: number
+    Returns:
+        sum of two numbers
+"""
 
 
 def add(a: float, b: float) -> float:
