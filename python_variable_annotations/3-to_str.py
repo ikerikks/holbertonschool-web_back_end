59af2730ccb5f794c93f1020ed41681816860ba5
ikerikks <ikerikks@gmail.com>
2023-09-22 10:40:26 +0200

    documentation

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+"""
+    to_str function
+    Args:
+        n: number
+    Returns:
+        number in string format
+"""
+
+
 def to_str(n: float) -> str:
     return str(n)
