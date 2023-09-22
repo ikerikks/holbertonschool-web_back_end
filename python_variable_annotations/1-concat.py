d0df4d913d5dd64bfacf566f1e4a32635dca6f26
ikerikks <ikerikks@gmail.com>
2023-09-22 10:35:41 +0200

    documentation

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+"""
+    concatenate two strings
+    Args:
+        str1: string
+        str2: string
+    Returns:
+        string
+"""
+
+
 def concat(str1: str, str2: str) -> str:
     return str1 + str2
