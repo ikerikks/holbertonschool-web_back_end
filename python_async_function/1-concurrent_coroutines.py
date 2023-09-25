6f304be5f104e7f850f65b17dcfbb4083428c163
ikerikks <ikerikks@gmail.com>
2023-09-25 14:15:03 +0200

    def doc

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    displays a list of random numbers
+    '''
     result = []
     for num in range(n):
         randomNumber = await wait_random(max_delay)
