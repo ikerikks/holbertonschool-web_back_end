1981deebbe08b943b50f1809a9ba06c8180f7106
ikerikks <ikerikks@gmail.com>
2023-09-26 13:57:30 +0200

    module file

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension():
+    return [rand async for rand in async_generator()]
