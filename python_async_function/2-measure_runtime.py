709aa8cba2b619bd360644805f96a6ba0f94aa84
ikerikks <ikerikks@gmail.com>
2023-09-26 10:51:52 +0200

    pycodestyle

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -12,12 +12,14 @@ async def measure_time(n: int, max_delay: int) -> float:
     Measure the total execution time
     '''
     start_time = time.time()
-    await wait_n(n, max_delay)  # Call the wait_n function from the other module
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     total_time = end_time - start_time
-    return total_time / n
+    average_time = float(total_time / n)
+    return average_time
 
 if __name__ == "__main__":
     n = 5
     max_delay = 9
-    print(measure_time(n, max_delay))
+    asyncio.run(measure_time(n, max_delay))
+    
