8c99a50a4940af7ed433fbe982a2f12cb458ab13
ikerikks <ikerikks@gmail.com>
2023-09-26 11:41:46 +0200

    annotation update

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ async generator
 '''
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None]:
     '''
     generates a random number
     '''
