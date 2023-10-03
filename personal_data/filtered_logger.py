99d0b86c53b384e996a303dec51f4742899dd689
ikerikks <ikerikks@gmail.com>
2023-10-03 15:29:40 +0200

    update

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,4 +10,4 @@ def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
     pattern = re.compile(rf'({"|".join(fields)})=[^{separator}]+')
-    return pattern.sub(rf"\1={redaction}", message)
+    return re.sub(pattern, rf"\1={redaction}", message)
