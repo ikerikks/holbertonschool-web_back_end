bf87fe0b3c9d646d91f4cf13f31985278c024a4b
ikerikks <ikerikks@gmail.com>
2023-10-03 15:18:38 +0200

    regex-ing

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+logger module
+"""
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    pattern = re.compile(rf'({"|".join(fields)})=[^{separator}]+')
+    return pattern.sub(rf"\1={redaction}", message)
