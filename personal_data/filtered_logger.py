28674ec05603b86527d280b4056d63e1006aa44b
ikerikks <ikerikks@gmail.com>
2023-10-04 17:55:02 +0200

    code update

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,12 +4,49 @@ logger module
 """
 import re
 from typing import List
+import logging
+
+
+PII_FIELDS = ("name", "ssn", "email", "phone", "creditcard")
 
 
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
-    '''filter data
-    '''
+    """filter data"""
     pattern = re.compile(rf'({"|".join(fields)})=[^{separator}]+')
     return re.sub(pattern, rf"\1={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format data filtered"""
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+        )
+        return super().format(record)
+
+
+def get_logger() -> logging.Logger:
+    """Create and configure a logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    # Create a StreamHandler with RedactingFormatter
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+
+    logger.addHandler(stream_handler)
+
+    return logger
