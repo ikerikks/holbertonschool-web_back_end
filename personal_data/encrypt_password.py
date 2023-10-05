511067d1ebf1dfff69d50669e37bb605cbcd9b34
ikerikks <ikerikks@gmail.com>
2023-10-05 12:56:50 +0200

    update

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,8 +3,13 @@
 import bcrypt
 
 
-def hash_password(password):
-    # Generate a salt and hash the password
+def hash_password(password: str) -> bytes:
+    """ Generate hash of a password """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check password matching with hashed password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
