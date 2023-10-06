8f0a3e9a5729015a7ff99a64fd819c19b7670ff5
ikerikks <ikerikks@gmail.com>
2023-10-06 14:12:51 +0200

    caching classes

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Put an item """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            removed_key = self.queue.pop(0)
+            del self.cache_data[removed_key]
+            print(f"DISCARD: {removed_key}\n")
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item from the cache """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,24 @@
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Put a key/value pair """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_item_key = list(self.cache_data.keys())[-1]
+            print(f"DISCARD: {last_item_key}\n")
+            del self.cache_data[last_item_key]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get a key/value pair """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
