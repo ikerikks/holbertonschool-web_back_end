4b4326c8be731477da3fd442e1a2f44cb6a56073
ikerikks <ikerikks@gmail.com>
2023-10-06 14:55:54 +0200

    update

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -8,32 +8,28 @@ class MRUCache(BaseCaching):
     """ mru cache class """
     def __init__(self):
         super().__init__()
+        self.accessed_items = {}
+        self.access_counter = 0
 
     def put(self, key, item):
-        """ Put a key/value pair into the cache """
+        """ put a key/value pair into the cache """
         if key is None or item is None:
             return
 
-        # Check if the cache is full
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # Find and remove the most recently used item
-            mru_key = max(self.accessed.keys(), key=lambda k: self.accessed[k])
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            mru_key = max(self.accessed_items, key=self.accessed_items.get)
             del self.cache_data[mru_key]
-            del self.accessed[mru_key]
             print(f"DISCARD: {mru_key}")
 
-        # Add the new item to the cache
+        self.accessed_items[key] = self.access_counter
         self.cache_data[key] = item
-        self.accessed[key] = self.current_time
-
-        self.current_time += 1
+        self.access_counter += 1
 
     def get(self, key):
-        """ Get the item from the cache"""
+        """ get a key/value pair """
         if key is None or key not in self.cache_data:
             return None
 
-        self.accessed[key] = self.current_time
-        self.current_time += 1
-
+        self.accessed_items[key] = self.access_counter
+        self.access_counter += 1
         return self.cache_data[key]
