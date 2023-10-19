c1ffb3be82ae433d50478c8f569721ae2ab5a416
ikerikks <ikerikks@gmail.com>
2023-10-19 12:58:14 +0200

    pycode style

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -61,7 +61,7 @@ def get_db() -> MySQLConnection:
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    
+
     db = mysql.connector.connect(
         user=username,
         password=password,
