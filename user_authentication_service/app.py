2d6573c65fed8ba9204b8e26e17c8b9f6d4f21f3
ikerikks <ikerikks@gmail.com>
2023-10-20 12:44:53 +0200

    log in

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ basic flask app
 """
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from auth import Auth
+import uuid
+
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -25,5 +27,36 @@ def register_user():
         return jsonify(response)
 
 
-if __name__ == "__main__":
+users_db = {
+    "bob@bob.com": {"email": "bob@bob.com", "password": "mySuperPwd"},
+}
+
+active_sessions = {}
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    data = request.form
+
+    if "email" not in data or "password" not in data:
+        return abort(400)
+
+    user_email = data["email"]
+    user_password = data["password"]
+
+    user = users_db.get(user_email)
+
+    if user is None or user["password"] != user_password:
+        return abort(401)
+
+    # Create a new session for the user
+    session_id = str(uuid.uuid4())
+    active_sessions[session_id] = user
+
+    response = jsonify({"email": user_email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
+
+
+if __name__ == '__main__':
     app.run(debug=True)
