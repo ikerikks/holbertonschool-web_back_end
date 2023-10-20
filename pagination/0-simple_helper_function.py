dfbe8d534d092cb3827a72120f891164ea412c63
ikerikks <ikerikks@gmail.com>
2023-10-20 10:25:35 +0200

    module

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr.bin/env python3
+""" simple module """
+
+
+def index_range(page, page_size):
+    """returns a list of page"""
+
+    if page <= 0 or page_size <= 0:
+        return (0, 0)
+
+    start = (page - 1) * page_size
+    end = start + page_size
+
+    return (start, end)
