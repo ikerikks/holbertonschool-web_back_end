bf551459274dd3b6109a32be5588782a5c18fa42
ikerikks <ikerikks@gmail.com>
2023-10-20 13:42:52 +0200

    session id

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -70,6 +70,17 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get user from session id"""
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by_session_id(session_id)
+            return user
+        except NoResultFound:
+            return None
+
 
 def _generate_uuid() -> str:
     """Generate a new UUID
