ccd0b765eee4dbcf5c4d909335933ddda5768c7c
ikerikks <ikerikks@gmail.com>
2023-11-01 13:47:11 +0100

    update

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -21,13 +21,23 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """Returns locale  string"""
+    """
+    Match user's language based on browser settings.
+
+    Returns:
+        str: The selected language/locale code.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
-    """Returns html"""
+    """
+    Render the main page of the application.
+
+    Returns:
+        str: The HTML content for the main page.
+    """
     return render_template('3-index.html', title=_('home_title'),
                            header=_('home_header'))
 
