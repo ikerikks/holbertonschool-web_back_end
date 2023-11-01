2f45d383d4961b359b568a70d4f316afbad56632
ikerikks <ikerikks@gmail.com>
2023-11-01 12:27:28 +0100

    update

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -7,9 +7,20 @@ app = Flask(__name__)
 
 babel = Babel(app)
 
+
+@app.route('/')
+def index():
+    return '<title>Welcome to Holberton</title>'
+
+
 class Config:
+    """ Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
+if __name__ == '__main__':
+    app.run(debug=True)
