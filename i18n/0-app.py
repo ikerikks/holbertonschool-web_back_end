92f856aaea51f44487c0f0ddac7304eee7f26ff2
ikerikks <ikerikks@gmail.com>
2023-11-01 12:09:15 +0100

    update

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """Returns html"""
+    return '<title>Welcome to Holberton</title>'
 
 
 if __name__ == '__main__':
