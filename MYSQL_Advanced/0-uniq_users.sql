f8bcd837fc7dcd95007da770f94de497df5e43d5
ikerikks <ikerikks@gmail.com>
2023-11-02 12:38:20 +0100

    delete

--- MYSQL_Advanced/0-uniq_users.sql
+++
@@ -1,9 +0,0 @@
--- Check if the table exists and drop it if it does (to handle scenarios where the table already exists)
-DROP TABLE IF EXISTS users;
-
--- Create the "users" table
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
