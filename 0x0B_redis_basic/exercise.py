6f57bd0218083c126df8b132f5235ddfee72c6ca
ikerikks <ikerikks@gmail.com>
2023-11-02 09:28:12 +0100

    py code

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" redis basic"""
+import redis
+import uuid
+
+class Cache:
+    """ Cache"""
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> str:
+        """ Store data"""
+        key = str(uuid.uuid4())
+        if isinstance(data, (str, bytes, int, float)):
+            self._redis.set(key, data)
+            return key
+        else:
+            raise ValueError("Data must be a string, bytes, int, or float.")
