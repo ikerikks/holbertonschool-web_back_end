1e16cef436e449ca0b6cbe0b1d0e341669a124ec
ikerikks <ikerikks@gmail.com>
2023-11-02 12:56:25 +0100

    update

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -9,6 +9,11 @@ BEGIN
     FROM corrections
     WHERE user_id = user_id;
     
+    -- Check if the user has only one project
+    IF (SELECT COUNT(DISTINCT project_id) FROM corrections WHERE user_id = user_id) = 1 THEN
+        SET average = (SELECT MAX(score) FROM corrections WHERE user_id = user_id);
+    END IF;
+    
     -- Update the user's average score
     UPDATE users
     SET average_score = average
