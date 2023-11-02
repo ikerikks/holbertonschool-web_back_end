1bf3fe06ad23fb656771be5fba31c7fc8af0c5b1
ikerikks <ikerikks@gmail.com>
2023-11-02 12:41:43 +0100

    doc

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,7 @@
+-- Check if the table exists and drop it if it does (to handle scenarios where the table already exists)
 DROP TABLE IF EXISTS users;
 
+-- Create the "users" table
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
