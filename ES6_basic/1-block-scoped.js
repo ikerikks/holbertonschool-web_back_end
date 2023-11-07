5fe38d832f22013639ce620bf8a11e78bd5a5532
ikerikks <ikerikks@gmail.com>
2023-11-07 15:15:24 +0100

    condition

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,6 +1,11 @@
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
+  
+ if (trueOrFalse) {
+    let task3 = true;
+    let task4 = false;
+  }
 
   return [task, task2];
 }
