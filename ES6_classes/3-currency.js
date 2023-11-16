5f6eb71d592000bfe7b569d688133d1c9b7ac0ba
ikerikks <ikerikks@gmail.com>
2023-11-16 16:09:01 +0100

    add blank lines

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -5,11 +5,13 @@ export default class Currency {
   }
 
   get code() { return this._code; }
+  
   set code(value) {
     this._code = value;
   }
 
   get name() { return this._name; }
+  
   set name(value) {
     this._name = value;
   }
