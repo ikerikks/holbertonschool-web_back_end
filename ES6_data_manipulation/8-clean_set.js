ee7cc6ba67e33f82994c88474ef9f4eb2346808f
ikerikks <ikerikks@gmail.com>
2023-11-23 19:44:07 +0100

    code structure update

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,7 @@
 export default function cleanSet(set, startString) {
-  return new Array(...set).filter(word => word.includes(startString)).map((str) => str.split(startString).join('')).join('-');
+  if(startString) {
+    return new Array(...set).filter(word => word.includes(startString)).map((str) => str.split(startString).join('')).join('-');
+  } else {
+    return '';
+  }
 }
