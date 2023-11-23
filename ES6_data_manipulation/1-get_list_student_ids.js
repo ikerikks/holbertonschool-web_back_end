f469e3405bfc77f84a69dcd54a7c6640e16dfd09
ikerikks <ikerikks@gmail.com>
2023-11-23 12:00:27 +0100

    parenthesize argument arrow function

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,3 @@
 export default function getStudentIds(array) {
-  return Array.isArray(array) ? array.map(student => student.id) : [];
+  return Array.isArray(array) ? array.map((student) => student.id) : [];
 }
