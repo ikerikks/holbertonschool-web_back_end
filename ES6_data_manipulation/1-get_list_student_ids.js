986e77017d85b5c583f0cd1154f04bf694e2cd93
ikerikks <ikerikks@gmail.com>
2023-11-23 10:23:53 +0100

    resolve git problem

--- ES6_data_manipulation/1-get_list_student_ids.js
+++
@@ -1,3 +0,0 @@
-export default function getListStudentsIds(array) {
-  return Array.isArray(array) ? array.map(item => item.id):[];
-}
--- ES6_data_manipulation/3-get_ids_sum.js
+++
@@ -1,5 +0,0 @@
-export default function getStudentIdsSum(array) {
-  return array.reduce((total, student) => {
-    return total + student.id; 
-  }, 0)
-}
