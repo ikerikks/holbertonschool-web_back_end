670da4b17c1d428378eb9a2b7c50b638c9f3b475
ikerikks <ikerikks@gmail.com>
2023-11-24 13:12:18 +0100

    update code

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,7 @@
 export default function hasValuesFromArray(set, array) {
-  return array.reduce((hasValues, value) => {
-    hasValues = set.has(value);
-    return hasValues;
-  }, false);
+  let result = false;
+  array.forEach(ele => {
+    result = set.has(ele);
+  });
+  return result;
 }
