294265592fccb44487d542a3245a32f8c9cc8164
ikerikks <ikerikks@gmail.com>
2023-11-27 16:18:20 +0100

    js methods combined

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(studentsList, city, newGrades) {
+  return studentsList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchedGrade = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: matchedGrade ? matchedGrade.grade : 'N/A',
+      };
+    });
+  }
+      
