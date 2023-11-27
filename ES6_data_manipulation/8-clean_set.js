391c21e7784b2d959cdde2f05fe9dc40693bc67b
ikerikks <ikerikks@gmail.com>
2023-11-27 12:43:36 +0100

    method condition update

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (typeof startString === 'string' && startString.length > 0) {
-    return [...set].filter((str) => str.startsWith(startString))
+    return [...set].filter((str) => str !== undefined && str.startsWith(startString))
     .map((str) => str.slice(startString.length)).join('-');
   }
   
