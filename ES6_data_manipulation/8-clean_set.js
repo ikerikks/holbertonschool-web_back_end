2062f2a4b3786b4cfe8fc66a0b64fb56bbe8ef05
ikerikks <ikerikks@gmail.com>
2023-11-27 12:44:36 +0100

    syntax update

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
   if (typeof startString === 'string' && startString.length > 0) {
     return [...set].filter((str) => str !== undefined && str.startsWith(startString))
-    .map((str) => str.slice(startString.length)).join('-');
+      .map((str) => str.slice(startString.length)).join('-');
   }
-  
+
   return '';
 }
