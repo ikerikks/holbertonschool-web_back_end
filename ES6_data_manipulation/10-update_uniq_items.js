1164f06a06ca48327b8056a705a6743725f51c34
ikerikks <ikerikks@gmail.com>
2023-11-27 11:32:54 +0100

    update of code structure

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,11 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error("Cannot process");
+  }
+
   map.forEach((value, key) => {
     value === 1 ? map.set(key, 100): value;
   });
-
+  
   return map;
 }
