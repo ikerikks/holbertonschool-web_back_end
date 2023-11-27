b48f3b98c0ceb0e55abe2bd74a0b8c1c72d91a29
ikerikks <ikerikks@gmail.com>
2023-11-27 11:45:26 +0100

    using if statement instead of ternary operation

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -4,7 +4,9 @@ export default function updateUniqueItems(map) {
   }
 
   map.forEach((value, key) => {
-    value === 1 ? map.set(key, 100) : null;
+    if (value === 1) {
+      map.set(key, 100);
+    }
   });
 
   return map;
