db9e23afd6e0f1d3fd2f412772a6e4cbe41dc2f9
ikerikks <ikerikks@gmail.com>
2023-11-30 16:40:24 +0100

    syntax correction

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
 function countStudents(path) {
   try {
-    let data = fs.readFileSync(`${path}`, 'utf-8').trim().split('\n');
+    const data = fs.readFileSync(`${path}`, 'utf-8').trim().split('\n');
     // console.log(data);
     data.shift();
-    let studentsCS = data.filter(val => val.includes('CS')).map((student) => student.split(','));
-    let studentsSWE = data.filter(val => val.includes('SWE')).map((student) => student.split(','));
+    const studentsCS = data.filter((val) => val.includes('CS')).map((student) => student.split(','));
+    const studentsSWE = data.filter((val) => val.includes('SWE')).map((student) => student.split(','));
 
     console.log(`Number of students: ${data.length}`);
     console.log(
-      `Number of students in CS: ${studentsCS.length}. ` + 
-      `List: ${studentsCS.map((val) => val[0]).join(', ')}`
+      `Number of students in CS: ${studentsCS.length}. `
+      + `List: ${studentsCS.map((val) => val[0]).join(', ')}`
     );
     console.log(
-      `Number of students in SWE: ${studentsSWE.length}. ` +
-      `List: ${studentsSWE.map((val) => val[0]).join(', ')}`
+      `Number of students in SWE: ${studentsSWE.length}. `
+      + `List: ${studentsSWE.map((val) => val[0]).join(', ')}`
     );
   } catch(error) {
-      throw new Error('Cannot load the database');
-    }
-
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
