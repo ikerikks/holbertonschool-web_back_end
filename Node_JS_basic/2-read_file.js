95228a385ade388305ad7e32054fffa76feaf1c3
ikerikks <ikerikks@gmail.com>
2023-11-30 16:44:49 +0100

    syntax correction

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -11,10 +11,12 @@ function countStudents(path) {
     console.log(`Number of students: ${data.length}`);
     console.log(
       `Number of students in CS: ${studentsCS.length}. `
-      + `List: ${studentsCS.map((val) => val[0]).join(', ')}`);
+      + `List: ${studentsCS.map((val) => val[0]).join(', ')}`,
+      );
     console.log(
       `Number of students in SWE: ${studentsSWE.length}. `
-      + `List: ${studentsSWE.map((val) => val[0]).join(', ')}`);
+      + `List: ${studentsSWE.map((val) => val[0]).join(', ')}`,
+    );
   } catch (error) {
     throw new Error('Cannot load the database');
   }
