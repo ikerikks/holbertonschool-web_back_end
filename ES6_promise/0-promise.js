c1546ee66d98d2843d2020d722a97574e8970470
ikerikks <ikerikks@gmail.com>
2023-12-04 16:03:02 +0100

    eslint fix

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if(success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
