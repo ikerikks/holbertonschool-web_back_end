dfb0e0780b68b3111cdcfd466c6e95fa8f26ef48
ikerikks <ikerikks@gmail.com>
2023-12-11 17:42:14 +0100

    update

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then(console.log);
+  const firstPromise = signUpUser(firstName, lastName);
+  const secondPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([firstPromise, secondPromise]);
 }
