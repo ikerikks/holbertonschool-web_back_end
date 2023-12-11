e935c1aed6adeb69805400b11125ec3b982f2ddb
ikerikks <ikerikks@gmail.com>
2023-12-11 16:44:25 +0100

    update

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,17 +1,13 @@
-// 3-all.js
-import { uploadPhoto, createUser } from './utils.js   ';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const mergePromise = Promise.all([uploadPhoto(), createUser()]);
 
-  return Promise.resolve()
-    .then(() => Promise.all([uploadPhoto(), createUser()]))
-    .then(([dataPhoto, dataUser]) => { 
-      console.log(dataPhoto.body + ' ' + dataUser.firstName + ' ' + dataUser.lastName);
+  return mergePromise
+    .then(([dataPhoto, dataUser]) => {
+      console.log(`${dataPhoto.body} ${dataUser.firstName} ${dataUser.lastName}`);
     })
-    .catch(() => { 
+    .catch(() => {
       console.log('Signup system offline');
-      throw new Error('Signup system offline');
-  });
-
+    });
 }
