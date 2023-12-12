bae1e0bb4576aed36c7df483a6fddf764e84eb60
ikerikks <ikerikks@gmail.com>
2023-12-12 19:23:06 +0100

    getters and setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,38 +1,33 @@
-export default class HolbertonCourse {  
+export default class HolbertonCourse {
   constructor(name, length, students) {
-    if ( typeof name === 'string' || typeof length === 'number' 
-      || Array.isArray(students)) {
-      this._name = name;
-      this._length = length;
-      this._students = students;
-      } else {
-      throw new TypeError();
+    if (typeof (name) === 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof (length) === 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
     }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() { return this._name; }
 
-  set name(name) { 
-    if (typeof name !== 'string') {
-      throw new TypeError ('Name must be a string');
-    }
-    this._name = name;
+  set name(value) {
+    this._name = value;
   }
 
   get length() { return this._length; }
 
   set length(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    } 
     this._length = length;
   }
 
   get students() { return this._students; }
 
-  set students(students) { 
-    if (!Array.isArray(students)) {
-      throw new TypeError('students must be an array');
-    }
-    this._students = students; }
+  set students(students) {
+    this._students = students;
+  }
 }
