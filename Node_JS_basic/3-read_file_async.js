f4b7fe96490d1343f1080433aef7079b9a49c880
ikerikks <ikerikks@gmail.com>
2023-12-15 16:08:23 +0100

    read file asynchronously

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  const promesa = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, res) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(res);
+    });
+  });
+
+  promesa
+    .then((data) => {
+      const strData = data.trim().split('\n');
+      strData.shift();
+      // console.log(data);
+      const studentsCS = strData.filter((val) => val.includes('CS')).map((student) => student.split(','));
+      const studentsSWE = strData.filter((val) => val.includes('SWE')).map((student) => student.split(','));
+
+      console.log(`Number of students: ${strData.length}`);
+      console.log(
+        `Number of students in CS: ${studentsCS.length}. `
+        + `List: ${studentsCS.map((val) => val[0]).join(', ')}`,
+      );
+      console.log(
+        `Number of students in SWE: ${studentsSWE.length}. `
+        + `List: ${studentsSWE.map((val) => val[0]).join(', ')}`,
+      );
+    })
+    .catch((err) => {
+      throw new Error('Cannot load the database');
+    });
+  return promesa;
+}
+
+module.exports = countStudents;
