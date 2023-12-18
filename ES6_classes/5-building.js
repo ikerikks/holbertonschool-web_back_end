cc5403c72ef6864bc0be9d7d79e4f2c7e62b0135
ikerikks <ikerikks@gmail.com>
2023-12-18 17:32:02 +0100

    code update

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error("Abstract class 'Building' cannot be instantiated.");
+    if (!(this.constructor === Building)) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
   get sqft() { return this._sqft; }
 
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
