8035d246791e6459b7ccca0fe9a874e79f44d053
ikerikks <ikerikks@gmail.com>
2024-05-28 22:31:16 +0200

    update

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,24 +1,12 @@
--- Create the stored procedure ComputeAverageScoreForUser
+-- creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INTEGER)
 BEGIN
-    DECLARE average FLOAT;
-    
-    -- Calculate the average score for the user
-    SELECT AVG(score) INTO average
-    FROM corrections
-    WHERE user_id = user_id;
-    
-    -- Check if the user has only one project
-    IF (SELECT COUNT(DISTINCT project_id) FROM corrections WHERE user_id = user_id) = 1 THEN
-        SET average = (SELECT MAX(score) FROM corrections WHERE user_id = user_id);
-    END IF;
-    
-    -- Update the user's average score
-    UPDATE users
-    SET average_score = average
-    WHERE id = user_id;
-    
+	UPDATE users
+	SET average_score = (SELECT AVG(score) FROM corrections AS cor
+						 WHERE cor.user_id = user_id)
+	WHERE id = user_id;
 END;
 //
 DELIMITER ;
