bddcf05e719624a5e2b4c786e075eb7d20855c13
ikerikks <ikerikks@gmail.com>
2024-05-28 15:24:25 +0200

    logout method

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ basic flask app
 """
-from flask import Flask, request, jsonify, abort, make_response
+from flask import Flask, request, jsonify, abort, make_response, redirect
 import uuid
 from auth import Auth
 from db import DB
@@ -27,24 +27,19 @@ def register_user():
         response = {"message": "email already registered"}
         return jsonify(response)
 
-
 @app.route("/sessions", methods=["POST"])
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
 
     try:
-        # Fetch user from the database
         user = db.find_user_by(email=email)
 
-        # Check if the password matches
         if user.hashed_password != password:
             abort(401)
 
-        # Create a new session ID
         session_id = db.create_session(user.email)
 
-        # Create the response with the session ID cookie
         response = make_response(jsonify({"email": user.email, "message": "logged in"}))
         response.set_cookie("session_id", session_id)
 
@@ -52,6 +47,22 @@ def login():
 
     except NoResultFound:
         abort(401)
+    
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    session_id = request.cookies.get('session_id')
+    
+    if not session_id:
+        abort(403)
+    
+    try:
+        db.delete_session(session_id)
+        
+        return redirect('/')
+    
+    except NoResultFound:
+        abort(403)
+
 
 
 if __name__ == "__main__":
