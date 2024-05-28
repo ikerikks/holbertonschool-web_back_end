9fb07abea65c7bf175efc4b157c53d61354c0be1
ikerikks <ikerikks@gmail.com>
2024-05-28 15:11:29 +0200

    update

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ basic flask app
 """
-from flask import Flask, request, jsonify, abort,  make_response
+from flask import Flask, request, jsonify, abort, make_response
 import uuid
 from auth import Auth
 from db import DB
@@ -28,30 +28,31 @@ def register_user():
         return jsonify(response)
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route("/sessions", methods=["POST"])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    
+    email = request.form.get("email")
+    password = request.form.get("password")
+
     try:
         # Fetch user from the database
         user = db.find_user_by(email=email)
-        
+
         # Check if the password matches
         if user.hashed_password != password:
             abort(401)
-        
+
         # Create a new session ID
         session_id = db.create_session(user.email)
-        
+
         # Create the response with the session ID cookie
         response = make_response(jsonify({"email": user.email, "message": "logged in"}))
-        response.set_cookie('session_id', session_id)
-        
+        response.set_cookie("session_id", session_id)
+
         return response
-    
+
     except NoResultFound:
         abort(401)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
