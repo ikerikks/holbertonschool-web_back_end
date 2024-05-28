226ab5f2776df78431e3a05dbeadf5b91680f12c
ikerikks <ikerikks@gmail.com>
2024-05-28 22:22:21 +0200

    update

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
--- Rank country origins of bands by the number of non-unique fans
-SELECT origin, SUM(nb_fans) as nb_fans
+-- ranks country origins of bands
+-- ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
--- List Glam rock bands ranked by their longevity
-SELECT band_name, 
-       CASE 
-         WHEN split = 'now' THEN 0
-         ELSE CAST(split AS SIGNED) - CAST(formed AS SIGNED)
-       END as lifespan
+-- lists all bands with Glam rock as their main style
+--  ranked by their longevity
+SELECT band_name, COALESCE(split, 2023) - formed AS lifespan
 FROM metal_bands
-WHERE split IS NOT NULL
-AND band_style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
