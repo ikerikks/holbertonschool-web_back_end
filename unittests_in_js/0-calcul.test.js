3707812e78274e8dea420c34d4f8846498a0f733
ikerikks <ikerikks@gmail.com>
2024-06-24 12:11:06 +0200

    update

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,6 +1,7 @@
 import { calculateNumber } from './0-calcul.js';
 import assert from 'assert';
 
+
 describe('calculateNumber', () => {
   it('should return the sum of an operation', () => {
     assert(calculateNumber(2, 2), 4);
