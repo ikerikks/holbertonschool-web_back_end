0f7c4d563de922f1449d044fdb8368dcbc077df3
ikerikks <ikerikks@gmail.com>
2024-06-25 10:33:56 +0200

    js test file

---
+++ unittests_in_js/1-calcul.js
@@ -0,0 +1,19 @@
+function calculateNumber(type, a, b) {
+  let result = '';
+  switch(type) {
+    case 'SUM':
+      result = Math.ceil(a + b);
+    break;
+
+    case 'SUBTRACT':
+      result = ((a - b) >= 0 || -1) * 
+      Math.ceil(Math.abs(a - b));
+    break;
+    
+    case 'DIVIDE':
+      result = Math.round((a / b) * 10) / 10 ;
+    break;
+  }
+  return result == Infinity || isNaN(result)?
+    'Error': result;
+}
