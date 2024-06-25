41759d992b629b41bed926ef46cc94dc0206b9d8
ikerikks <ikerikks@gmail.com>
2024-06-25 11:39:55 +0200

    update

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
+const require = require('assert')
 const { calculateNumber } = require('./0-calcul.js');
-
 describe('calculateNumber', () => {
   it('should return the sum of an operation', () => {
     assert(calculateNumber(2, 2), 4);
