9c37455d3380e886a076af80dfc45bd525bdef2a
ikerikks <ikerikks@gmail.com>
2024-07-02 18:37:02 +0200

    fix

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -2,14 +2,15 @@
 -- the first by the second number
 -- or returns 0 if the second number is equal to 0.
 DELIMITER //
+
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS DECIMAL(10, 6)
 DETERMINISTIC
 BEGIN
     IF b = 0 THEN
         RETURN 0;
     ELSE
-        RETURN a DIV b;
+        RETURN a / b;
     END IF;
 END //
 
