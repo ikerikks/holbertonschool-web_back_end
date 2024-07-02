7b1a8bd438e14c83afbbe63f2bbe71bcf62ec1fc
ikerikks <ikerikks@gmail.com>
2024-07-02 18:29:46 +0200

    fix

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,17 +1,16 @@
 -- creates a function SafeDiv that divides (and returns) 
 -- the first by the second number
 -- or returns 0 if the second number is equal to 0.
+DELIMITER //
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 2)
+RETURNS INT
 DETERMINISTIC
 BEGIN
-    DECLARE result DECIMAL(10, 2);
     IF b = 0 THEN
-        SET result = 0;
+        RETURN 0;
     ELSE
-        SET result = a / b;
+        RETURN a DIV b;
     END IF;
-    RETURN result;
 END //
 
 DELIMITER ;
