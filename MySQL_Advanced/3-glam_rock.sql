146956877cb9975fe084c6e60649a28cca3baff1
ikerikks <ikerikks@gmail.com>
2024-07-02 12:21:32 +0200

    fix

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,16 @@
 -- lists all bands with Glam rock as their main style
 --  ranked by their longevity
-SELECT band_name, COALESCE(split, 2023) - formed AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+-- 3-glam_rock.sql
+USE holberton;
+SELECT 
+    name AS band_name,
+    CASE
+        WHEN split IS NULL THEN YEAR(CURDATE()) - formed
+        ELSE split - formed
+    END AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    main_style = 'Glam rock'
+ORDER BY 
+    lifespan DESC;
