847c4d32c4aaf3f836b110c0074fd2d8aae27eb8
ikerikks <ikerikks@gmail.com>
2024-07-03 11:25:42 +0200

    fix

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,3 +1,3 @@
-const calculateNumber = (a, b) => Math.round(Math.abs(a)) + Math.round(Math.abs(b));
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 module.exports = { calculateNumber };
 
