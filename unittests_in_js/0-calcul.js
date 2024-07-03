a44ad1d71ccd768ed962ab19c76aca7743de7076
ikerikks <ikerikks@gmail.com>
2024-07-03 11:02:05 +0200

    fix

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,3 +1,7 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+function calculateNumber (a, b) {
+  a = Math.round(a);
+  b = Math.round(b);
+  return a + b;
+}
 module.exports = { calculateNumber };
 
