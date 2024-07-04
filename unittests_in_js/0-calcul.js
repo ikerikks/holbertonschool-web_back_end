b564eba05882468270d62f47a599b37abf40527f
ikerikks <ikerikks@gmail.com>
2024-07-04 13:58:43 +0200

    deletion folder

--- unittests_in_js/0-calcul.js
+++
@@ -1,7 +0,0 @@
-const calculateNumber = (a, b) =>  {
-  arg1 = Math.round(Math.abs(a)) * Math.sign(a);
-  arg2 = Math.round(Math.abs(b)) * Math.sign(b);
-  return arg1 + arg2;
-}
-module.exports = { calculateNumber };
-
