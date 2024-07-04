5270fce8d5d9b2b801383a434b8f153e9a183aa5
ikerikks <ikerikks@gmail.com>
2024-07-04 15:06:49 +0200

    fix

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,7 +1,10 @@
 const calculateNumber = (a, b) =>  {
-  // arg1 = Math.round(Math.abs(a)) * Math.sign(a);
-  // arg2 = Math.round(Math.abs(b)) * Math.sign(b);
-  return Math.round(arg1 + arg2);
+  arg1 = Math.round(Math.abs(a)) * Math.sign(a);
+  arg2 = Math.round(Math.abs(b)) * Math.sign(b);
+  if (isNaN(arg1) || isNaN(arg2)) {
+    return error;
+  }
+  return arg1 + arg2;
 }
 module.exports = calculateNumber;
 
