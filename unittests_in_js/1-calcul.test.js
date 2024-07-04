c0175ab50094f71527e8dff719b2e6fe6b796ac1
ikerikks <ikerikks@gmail.com>
2024-07-04 14:05:46 +0200

    unittest folder

---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { calculateNumber } = require('./1-calcul.js');
+
+describe('calculateNumber', () => {
+  it('return a sum', () => {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+  it('return a substraction)', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+  it('return a division', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it('return an error', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
