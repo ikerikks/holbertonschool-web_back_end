9a44d4b400ad292834c67933ac00704d0094f20c
ikerikks <ikerikks@gmail.com>
2024-08-07 18:52:47 +0200

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.resume();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim().split('').map((l, index)=>
+    index == 0 ?l.toUpperCase():l
+  ).join('');
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}\n`);
 
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
+  if (!process.stdin.isTTY) {
+    process.stdout.write("This important software is now closing\n");
   }
-});
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
