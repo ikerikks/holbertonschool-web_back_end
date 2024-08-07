15b05360f8dc2208a2065b2dca89f9e2601e3de2
ikerikks <ikerikks@gmail.com>
2024-08-07 19:13:49 +0200

    fix

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,31 +1,29 @@
-const http = require('http');
-const students = require('./3-read_file_async');
+const server = require('http');
+const process = require('process');
+const countStudents = require('./3-read_file_async');
 
+const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
-  switch (req.url) {
-    case '/':
-      res.writeHead(200);
-      res.end('Hello Holberton School!');
-      break;
-    case '/students':
-      res.writeHead(200);
-      res.write('This is the list of our students\n');
-      students(process.argv[2])
-        .then((data) => {
-          res.end(data);
-        })
-        .catch((error) => {
-          res.end(error.message);
-        });
-      break;
-    default:
-      res.writeHead(404);
-      res.end(JSON.stringify({ error: 'Resource not found' }));
+const app = server.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(`${data.join('\n')}`);
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
   }
 });
 
-app.listen(port);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
