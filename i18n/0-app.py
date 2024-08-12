c54ac9172124e46e1b0690a71545abd3dc00f9bc
ikerikks <ikerikks@gmail.com>
2024-08-12 14:54:50 +0200

    fix

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""Basic Flask app Module."""
-
+"""Main app"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Generate template"""
+    
     return render_template('0-index.html')
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
