745345c866ccf89046a8a8a67174e8e8e9f01a67
ikerikks <ikerikks@gmail.com>
2024-08-12 12:39:56 +0200

    fix

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -7,24 +7,29 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 class Config:
     """Configuration for language support"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @app.route('/')
 def hello():
     """Render a welcome message"""
     return render_template('3-index.html')
 
+
 # Define the locale selector function
 def get_locale():
     """Determine the best language for the user"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 # Set the locale selector function
 babel.locale_selector_func = get_locale
