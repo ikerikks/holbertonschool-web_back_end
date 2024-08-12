54c9399e9f5cce3ad56d8c5c37d980a2a42a0c0d
ikerikks <ikerikks@gmail.com>
2024-08-12 14:01:19 +0200

    fix

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -7,8 +7,10 @@ Basic Flask app Module.
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 babel = Babel(app)
+
 
 class Config:
     """Configuration class."""
@@ -16,17 +18,20 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome() -> str:
     """Endpoint returning Hello world."""
     return render_template("2-index.html")
 
-@babel.localeselector
+
 def locale_selector() -> str:
     """Select the best match language."""
     return request.accept_languages.best_match(["en", "fr"])
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
