fc3c8424607ad056b745aadfe7fd9db1e3b4314b
ikerikks <ikerikks@gmail.com>
2024-08-12 12:34:55 +0200

    update

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,35 +1,29 @@
-#!/usr/bin/env python3
-"""module application"""
-
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
 
-
-class Config():
-    """configuration language"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
+class Config:
+    """Configuration for language support"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 app.config.from_object(Config)
 babel = Babel(app)
-babel.init_app(app)
-
 
 @app.route('/')
 def hello():
-    """print hello world"""
-    return render_template("3-index.html")
+    """Render a welcome message"""
+    return render_template('3-index.html')
 
-
-@babel.localeselector
+# Define the locale selector function
 def get_locale():
-    """define the best language to be use"""
+    """Determine the best language for the user"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+# Set the locale selector function
+babel.locale_selector_func = get_locale
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
